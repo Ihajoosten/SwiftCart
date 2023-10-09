6fe2c922daf07ecc8041e1671bc2b3fbdb0e2531
Luc Joosten <lhajoosten@gmail.com>
2023-10-09 17:12:14 +0200

    Implemented Base ApplicationService

---
+++ Services/Product/Product.Application/Interfaces/Base/IApplicationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Application.Interfaces.Base
+{
+    public interface IApplicationService<TDto, TCreateDto, TUpdateDto>
+    {
+        Task<IEnumerable<TDto>> GetAllAsync();
+        Task<TDto?> GetByIdAsync(int id);
+        Task<TDto> CreateAsync(TCreateDto createDto);
+        Task UpdateAsync(int id, TUpdateDto updateDto);
+        Task DeleteAsync(int id);
+    }
+}
