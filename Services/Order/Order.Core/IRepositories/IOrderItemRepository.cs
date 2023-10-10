bd2e0366abc8f1d594eb66e18ee46d669c1fa19e
Luc Joosten <lhajoosten@gmail.com>
2023-10-10 10:23:05 +0200

    Implemented the Order Service repository interfaces

---
+++ Services/Order/Order.Core/IRepositories/IOrderItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using Order.Core.Entities;
+using Order.Core.IRepositories.Base;
+namespace Order.Core.IRepositories
+{
+    public interface IOrderItemRepository : IRepository<OrderItem>
+    {
+        Task<IEnumerable<OrderItem>> GetOrderItemsByOrderIdAsync(int orderId);
+        Task<IEnumerable<OrderItem>> GetTopSellingProductsAsync(int count);
+    }
+}
---
+++ Services/Order/Order.Core/IRepositories/IOrderStatusHistoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Order.Core.Entities;
+using Order.Core.IRepositories.Base;
+
+namespace Order.Core.IRepositories
+{
+    public interface IOrderStatusHistoryRepository : IRepository<OrderStatusHistory>
+    {
+        Task<IEnumerable<OrderStatusHistory>> GetStatusHistoryByOrderIdAsync(int orderId);
+        Task<OrderStatusHistory> GetLatestStatusByOrderIdAsync(int orderId);
+    }
+}
