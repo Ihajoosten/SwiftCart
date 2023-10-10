e937e58faea9d80e858153ab9de57ebab12ad7c3
Luc Joosten <lhajoosten@gmail.com>
2023-10-10 10:31:50 +0200

    implemented base ef repository<T>

---
+++ Services/Order/Order.Infrastructure/EFRepositories/Base/EFRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Core.IRepositories.Base;
+using Order.Infrastructure.Data.Interface;
+
+namespace Order.Infrastructure.EFRepositories.Base
+{
+    public class EFRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly IOrderContext _context;
+
+        public EFRepository(IOrderContext orderContext)
+        {
+            _context = orderContext ?? throw new ArgumentNullException(nameof(orderContext));
+        }
+
+        public async Task<IReadOnlyList<T?>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+
+        public async Task<T?> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
