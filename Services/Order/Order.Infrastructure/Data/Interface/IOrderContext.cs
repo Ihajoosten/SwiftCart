ad25bb786bfc22547a6e493bfb044d3cd086983e
Luc Joosten <lhajoosten@gmail.com>
2023-10-10 10:28:12 +0200

    Implemented the Order Context class

---
+++ Services/Order/Order.Infrastructure/Data/Interface/IOrderContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Order.Core.Entities;
+
+namespace Order.Infrastructure.Data.Interface
+{
+    public interface IOrderContext
+    {
+        DbSet<Core.Entities.Order> Products { get; set; }
+        DbSet<OrderItem> Brands { get; set; }
+        DbSet<OrderStatusHistory> Categories { get; set; }
+        DbSet<ShippingDetails> Reviews { get; set; }
+
+        Task<int> SaveChangesAsync();
+        DbSet<T> Set<T>() where T : class;
+        EntityEntry<T> Entry<T>(T entity) where T : class;
+    }
+}
---
+++ Services/Order/Order.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Order.Core.Entities;
+using Order.Infrastructure.Data.Interface;
+
+namespace Order.Infrastructure.Data
+{
+    public class OrderContext : DbContext, IOrderContext
+    {
+        public OrderContext() { }
+
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+
+        public DbSet<Core.Entities.Order> Products { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DbSet<OrderItem> Brands { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DbSet<OrderStatusHistory> Categories { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DbSet<ShippingDetails> Reviews { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override DbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+
+        public override EntityEntry<T> Entry<T>(T entity) where T : class
+        {
+            return base.Entry(entity);
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
