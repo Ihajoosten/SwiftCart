ceaca139107705292c7fd12df3ef38755792c5cc
Luc Joosten <lhajoosten@gmail.com>
2023-10-10 09:45:02 +0200

    Implemented ProductTag | ItemImage Controller

--- Services/Product/Product.Api/Controllers/ItemImageController.cs
+++ Services/Product/Product.Api/Controllers/ItemImageController.cs
@@ -1,12 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Product.Application.Dto.ItemImage;
+using Product.Application.Dto.ItemImage;
+using Product.Application.Interfaces;
 
 namespace Product.Api.Controllers
 {
-    public class ItemImageController : Controller
+    [ApiController]
+    [Route("api/itemimages")]
+    public class ItemImageController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IAppItemImageService _itemImageService;
+
+        public ItemImageController(IAppItemImageService itemImageService)
         {
-            return View();
+            _itemImageService = itemImageService ?? throw new ArgumentNullException(nameof(itemImageService));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllItemImages()
+        {
+            var products = await _itemImageService.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItemImageById(int id)
+        {
+            var product = await _itemImageService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItemImage([FromBody] CreateItemImageDto createDto)
+        {
+            var createdProduct = await _itemImageService.CreateAsync(createDto);
+            return CreatedAtAction(nameof(GetItemImageById), new { id = createdProduct.Id }, createdProduct);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItemImage(int id, [FromBody] UpdateItemImageDto updateDto)
+        {
+            await _itemImageService.UpdateAsync(id, updateDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItemImage(int id)
+        {
+            await _itemImageService.DeleteAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("product/{productId}/images")]
+        public async Task<IActionResult> GetImagesByProductId(int productId)
+        {
+            var images = await _itemImageService.GetImagesByProductIdAsync(productId);
+            return Ok(images);
         }
     }
 }
--- Services/Product/Product.Api/Controllers/ProductTagController.cs
+++ Services/Product/Product.Api/Controllers/ProductTagController.cs
@@ -1,12 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Product.Application.Dto.ProductTag;
+using Product.Application.Interfaces;
 
 namespace Product.Api.Controllers
 {
-    public class ProductTagController : Controller
+    [ApiController]
+    [Route("api/producttags")]
+    public class ProductTagController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IAppProductTagService _productTagService;
+
+        public ProductTagController(IAppProductTagService productTagService)
         {
-            return View();
+            _productTagService = productTagService ?? throw new ArgumentNullException(nameof(productTagService));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductTags()
+        {
+            var products = await _productTagService.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductTagById(int id)
+        {
+            var product = await _productTagService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductTag([FromBody] CreateProductTagDto createDto)
+        {
+            var createdProduct = await _productTagService.CreateAsync(createDto);
+            return CreatedAtAction(nameof(GetProductTagById), new { id = createdProduct.Id }, createdProduct);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProductTag(int id, [FromBody] UpdateProductTagDto updateDto)
+        {
+            await _productTagService.UpdateAsync(id, updateDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductTag(int id)
+        {
+            await _productTagService.DeleteAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("tag/{tagId}/products")]
+        public async Task<IActionResult> GetProductsByTag(int tagId)
+        {
+            var products = await _productTagService.GetProductsByTagAsync(tagId);
+            return Ok(products);
+        }
+
+        [HttpGet("product/{productId}/tags")]
+        public async Task<IActionResult> GetTagsByProduct(int productId)
+        {
+            var tags = await _productTagService.GetTagsByProductAsync(productId);
+            return Ok(tags);
         }
     }
 }
